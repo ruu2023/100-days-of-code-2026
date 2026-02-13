a75248fa827f2a934670479f173f5d42bfb0b173
ruu2023 <kokihiru2038@gmail.com>
2026-02-13 21:02:04 +0900

    update

--- cloudflare/src/app/page.tsx
+++ cloudflare/src/app/page.tsx
@@ -1,15 +1,9 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare";
-import Image from "next/image";
 import Client from "./client";
 
 export default async function Home() {
-	const { env } = getCloudflareContext();
-	const { results } = await env.DB.prepare("SELECT * FROM posts").all();
-
 	return (
 		<div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-
 				<Client />
 			</main>
 		</div>
