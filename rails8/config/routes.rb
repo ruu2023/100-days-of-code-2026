613c40110295b1f70af4dbf7352e07e27a60e628
ruu2023 <kokihiru2038@gmail.com>
2026-03-12 00:19:00 +0900

    update

--- rails8/config/routes.rb
+++ rails8/config/routes.rb
@@ -48,5 +48,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  get "/" => "rails/health#show", as: :rails_health_check
+  get "/", to: "posts#index"
 end
