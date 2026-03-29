190a04a7700c4fb72489676af9ca5df09f8067e1
ruu2023 <kokihiru2038@gmail.com>
2026-03-29 22:13:19 +0900

    updatea

--- rails8/config/initializers/omniauth.rb
+++ rails8/config/initializers/omniauth.rb
@@ -2,8 +2,8 @@
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
-           ENV["GOOGLE_CLIENT_ID"],
-           ENV["GOOGLE_CLIENT_SECRET"],
+           ENV["GOOGLE_OAUTH_CLIENT_ID"],
+           ENV["GOOGLE_OAUTH_CLIENT_SECRET"],
            {
              scope: ["email", "profile"],
              prompt: "select_account",
