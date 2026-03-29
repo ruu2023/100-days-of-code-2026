23bf42e4f5e910d7f51fa0461e834cf5985cf04f
ruu2023 <kokihiru2038@gmail.com>
2026-03-29 22:54:08 +0900

    update

---
+++ rails8/app/controllers/csv_tables_controller.rb
@@ -0,0 +1,18 @@
+class CsvTablesController < ApplicationController
+  def index
+    @csv_text = params[:csv_text] || ""
+    @parsed_rows = parse_csv(@csv_text) if @csv_text.present?
+  end
+
+  private
+
+  def parse_csv(text)
+    rows = CSV.read(text.gsub(/\r\n?/, "\n"), headers: true, skip_blanks: true)
+    {
+      headers: rows.headers,
+      rows: rows.map(&:fields)
+    }
+  rescue CSV::MalformedCSVError, ArgumentError
+    nil
+  end
+end
